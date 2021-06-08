9c3f4035c8e085b5178107d836f474e573219cea
yeremiadio <raikkonendio@gmail.com>
2021-06-08 11:15:51 +0700

    icon + add remove function on book

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@ const BOOK_ITEMID = "itemId";
 
 const searchBook = document.getElementById("searchBox").value;
 
+function searchFilter() {
+  var input, filter, inner, item, name, i;
+  input = document.getElementById("searchBox");
+  filter = input.value.toUpperCase();
+  item = document.getElementsByClassName("item");
+  inner = document.getElementsByClassName("inner");
+  for (i = 0; i < inner.length; i++) {
+    name = inner[i].getElementsByTagName("h2")[0];
+    nameValue = name.innerText;
+    if (nameValue.toUpperCase().indexOf(filter) > -1) {
+      item[i].style.display = "";
+    } else {
+      item[i].style.display = "none";
+    }
+  }
+}
+
 function makeBook(
   bookId,
   title,
@@ -26,25 +43,25 @@ function makeBook(
   const textTimestamp = document.createElement("p");
   textTimestamp.innerText = timestamp;
 
-  const idContainer = document.createElement("li");
-  idContainer.classList.add("id-book");
-  idContainer.append(bookID);
+  // const idContainer = document.createElement("li");
+  // idContainer.classList.add("id-book");
+  // idContainer.append(bookID);
   const bookContainer = document.createElement("li");
   bookContainer.classList.add("inner");
-  bookContainer.append(textTitle, textAuthor, textTimestamp);
+  bookContainer.append(bookID, textTitle, textAuthor, textTimestamp);
 
   const container = document.createElement("ul");
   container.classList.add("item", "shadowdiv");
-  container.append(idContainer, bookContainer);
+  container.append(bookContainer);
+
+  // const buttonCompleted = document.createElement("li");
+  // buttonCompleted.classList.add("buttonCompleted");
+  // buttonCompleted.append(createUndoButton(), createTrashButton());
 
   if (isCompleted) {
-    // container
-    //   .append
-    //   createUndoButton(),
-    //    createTrashButton()
-    //   ();
+    container.append(createUndoButton(), createTrashButton());
   } else {
-    container.append(createCheckButton());
+    container.append(createCheckButton(), createTrashButton());
   }
 
   return container;
@@ -58,7 +75,7 @@ function createUndoButton() {
 
 function createTrashButton() {
   return createButton("trash-button", function (event) {
-    removeTaskFromCompleted(event.target.parentElement);
+    removeButton(event.target.parentElement);
   });
 }
 
@@ -80,7 +97,6 @@ function createButton(buttonTypeClass /* string */, eventListener /* Event */) {
 
 function addBook() {
   const uncompletedBook = document.getElementById(UNCOMPLETED_BOOK_ID);
-  const listCompleted = document.getElementById(COMPLETED_BOOK_ID);
   var inputElement = document.getElementsByClassName("myCheck");
   const titleBook = document.getElementById("title").value;
   //   checkB = document.querySelector(".myCheck").value;
@@ -113,7 +129,7 @@ function addBook() {
 
 function addTaskToCompleted(taskElement) {
   const listCompleted = document.getElementById(COMPLETED_BOOK_ID);
-  const bookID = document.getElementById("id-book");
+  const bookID = taskElement.querySelector(".inner > p").innerText;
   const titleBook = taskElement.querySelector(".inner > h2").innerText;
   const authorBook = taskElement.querySelector(".inner > h4").innerText;
   const timestamp = taskElement.querySelector(".inner > p").innerText;
@@ -123,9 +139,35 @@ function addTaskToCompleted(taskElement) {
   book[BOOK_ITEMID] = booksingle.bookID;
   listCompleted.append(book);
   taskElement.remove();
-  
+
   updateDataToStorage();
   console.log("Dicentang");
+}
+
+function removeButton(taskElement /* HTMLELement */) {
+  const bookPosition = findBookIndex(taskElement[BOOK_ITEMID]);
+  books.splice(bookPosition, 1);
+
+  taskElement.remove();
+  updateDataToStorage();
+}
+
+function undoTaskFromCompleted(taskElement /* HTMLELement */) {
+  const listUncompleted = document.getElementById(UNCOMPLETED_BOOK_ID);
+  const bookID = taskElement.querySelector(".inner > p").innerText;
+  const titleBook = taskElement.querySelector(".inner > h2").innerText;
+  const authorBook = taskElement.querySelector(".inner > h4").innerText;
+  const timestamp = taskElement.querySelector(".inner > p").innerText;
+
+  const book = makeBook(bookID, titleBook, authorBook, timestamp, false);
+
+  const booksingle = findBook(taskElement[BOOK_ITEMID]);
+  booksingle.isCompleted = false;
+  book[BOOK_ITEMID] = booksingle.bookID;
+  listUncompleted.append(book);
+  taskElement.remove();
+
+  updateDataToStorage();
 }
 
 function refreshDataFromBooks() {
